86ae2368bb2d8e9b784e9f093aedb0e9dc34a9c9
rene069 <rene_rasmussen@live.dk>
2023-10-30 10:20:21 +0100

    Test Change

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@ void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   Serial.println("LED ON");
-  delay(500);                    // wait for a second
+  delay(1000);                    // wait for a second
   digitalWrite(LED_BUILTIN, LOW); // turn the LED off by making the voltage LOW
   Serial.println("LED OFF");
-  delay(500);                    // wait for a second
+  delay(1000);                    // wait for a second
 }
